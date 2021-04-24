3992b34fb3e358fb96ac1ca1e121df0025d4c12d
Student SUAS 5330 <studentUSB@suas.com>
2021-04-23 20:52:38 -0600

    straight line following complete and working.

--- algorithms.py
+++ algorithms.py
@@ -37,19 +37,12 @@ class Algorithms:
 
         # Unpackaging variables
         r_n = r[0][0]
-        print('r_n', r_n)
         r_e = r[1][0]
-        print('r_e', r_e)
         r_d = r[2][0]
-        print('r_d', r_d)
         p_n = p[0][0]
-        print('p_n', p_n)
         p_e = p[1][0]
-        print('p_e', p_e)
         p_d = p[2][0]
-        print('p_d', p_d)
         k_i = np.array([0, 0, 1])
-        print('k_i', k_i)
         e_i_p = np.array([[p_n-r_n], [p_e-r_e], [p_d-r_d]])  # 3x1
         print('e_i_p', e_i_p)
         normal = (np.cross(q.T, k_i)/(np.linalg.norm(np.cross(q.T, k_i)))).T  # 3x1
@@ -62,7 +55,7 @@ class Algorithms:
         q_n = q[0]
         q_e = q[1]
         q_d = q[2]
-        chi_q = np.atan2(q_e, q_n)
+        chi_q = np.arctan2(q_e, q_n)
         chi = chi
 
         c_n = c[0][0]
@@ -72,32 +65,30 @@ class Algorithms:
         k_orbit = k_orbit
         rho = rho
 
-
         if flag == 1:  # straight line
             pass
             # TODO Algorithm 3 goes here
-            h_d = -r_d + np.sqrt(s_n**2 + s_e**2) * (q_d/ np.sqrt(q_n**2 + q_e**2)) #equation 10.5
-            chi_q = np.atan2(q_e, q_n)
+            h_d = -r_d + np.sqrt(s_n**2 + s_e**2) * (q_d / np.sqrt(q_n**2 + q_e**2)) #equation 10.5
+            chi_q = np.arctan2(q_e, q_n)
             while (chi_q - chi) < -np.pi:
                 chi_q = chi_q + 2 * np.pi
             while (chi_q - chi) > np.pi:
                 chi_q = chi_q - 2 * np.pi
             e_py = -np.sin(chi_q) * (p_n - r_n) + np.cos(chi_q) * (p_e - r_e)
             e_crosstrack = e_py
-            chi_c = chi_q - chi_inf * 2 / np.pi * np.atan(k_path * e_py)
+            chi_c = chi_q - chi_inf * 2 / np.pi * np.arctan(k_path * e_py)
             h_c = h_d
-            
         elif flag == 2:  # orbit following
             pass
             # TODO Algorithm 4 goes here
             h_c = -c_d
             d = np.sqrt((p_n - c_n)**2 + (p_e - c_e)**2)
-            rho = np.atan2(p_e - c_e, p_n - c_n)
+            rho = np.arctan2(p_e - c_e, p_n - c_n)
             while (rho - chi) < -np.pi:
                 rho = rho + 2 * np.pi
             while (rho - chi) > np.pi:
                 rho = rho - 2 * np.pi
-            chi_c = rho + lamb * (np.pi / 2 + np.atan(k_orbit * ((d - p) / rho)))
+            chi_c = rho + lamb * (np.pi / 2 + np.arctan(k_orbit * ((d - p) / rho)))
             e_crosstrack = d - rho
         else:
             raise Exception("Invalid path type")
@@ -106,251 +97,257 @@ class Algorithms:
 
     # followWpp algorithm left here for reference
     # It is not used in the final implementation
-    def followWpp(self, w, p, newpath):
-        """
-        followWpp implements waypoint following via connected straight-line
-        paths.
-
-        Inputs:
-            w = 3xn matrix of waypoints in NED (m)
-            p = position of MAV in NED (m)
-            newpath = flag to initialize the algorithm or define new waypoints
-
-        Outputs
-            r = origin of straight-line path in NED (m)
-            q = direction of straight-line path in NED (m)
-
-        Example Usage;
-            r, q = followWpp(w, p, newpath)
-
-        Reference: Beard, Small Unmanned Aircraft, Chapter 11, Algorithm 5
-        Copyright 2018 Utah State University
-        """
-
-        if self.i is None:
-            self.i = 0
-
-        if newpath:
-            # initialize index
-            self.i = 1
-
-        # check sizes
-        m, N = w.shape
-        assert N >= 3
-        assert m == 3
-
-        # calculate the q vector
-        r = w[:, self.i - 1]
-        qi1 = s_norm(w[:, self.i], -w[:, self.i - 1])
-
-        # Calculate the origin of the current path
-        qi = s_norm(w[:, self.i + 1], -w[:, self.i])
-
-        # Calculate the unit normal to define the half plane
-        ni = s_norm(qi1, qi)
-
-        # Check if the MAV has crossed the half-plane
-        if in_half_plane(p, w[:, self.i], ni):
-            if self.i < (N - 2):
-                self.i += 1
-        q = qi1
-        return r, q
-
-    # followWppFillet algorithm left here for reference.
-    # It is not used in the final implementation
-    def followWppFillet(self, w, p, R, newpath):
-        """
-        followWppFillet implements waypoint following via straightline paths
-        connected by fillets
-
-        Inputs:
-            W = 3xn matrix of waypoints in NED (m)
-            p = position of MAV in NED (m)
-            R = fillet radius (m)
-            newpath = flag to initialize the algorithm or define new waypoints
-
-        Outputs
-            flag = flag for straight line path (1) or orbit (2)
-            r = origin of straight-line path in NED (m)
-            q = direction of straight-line path in NED (m)
-            c = center of orbit in NED (m)
-            rho = radius of orbit (m)
-            lamb = direction or orbit, 1 clockwise, -1 counter clockwise
-
-        Example Usage
-            [flag, r, q, c, rho, lamb] = followWppFillet( w, p, R, newpath )
-
-        Reference: Beard, Small Unmanned Aircraft, Chapter 11, Algorithm 6
-        Copyright 2018 Utah State University
-        """
-
-        if self.i is None:
-            self.i = 0
-            self.state = 0
-        if newpath:
-            # Initialize the waypoint index
-            self.i = 2
-            self.state = 1
-
-            # Check size of waypoints matrix
-            m, N = w.shape  # Where 'N' is the number of waypoints and 'm' dimensions
-            assert N >= 3
-            assert m == 3
-        else:
-            [m, N] = w.shape
-            assert N >= 3
-            assert m == 3
-        # Calculate the q vector and fillet angle
-        qi1 = mat(s_norm(w[:, self.i], -w[:, self.i - 1]))
-        qi = mat(s_norm(w[:, self.i + 1], -w[:, self.i]))
-        e = acos(-qi1.T * qi)
-
-        # Determine if the MAV is on a straight or orbit path
-        if self.state == 1:
-            c = mat([0, 0, 0]).T
-            rho = 0
-            lamb = 0
-
-            flag = 1
-            r = w[:, self.i - 1]
-            q = q1
-            z = w[:, self.i] - (R / (np.tan(e / 2))) * qi1
-            if in_half_plane(p, z, qi1):
-                self.state = 2
-
-        elif self.state == 2:
-            r = [0, 0, 0]
-            q = [0, 0, 0]
-
-            flag = 2
-            c = w[:, self.i] - (R / (np.sin(e / 2))) * s_norm(qi1, -qi)
-            rho = R
-            lamb = np.sign(qi1(1) * qi(2) - qi1(2) * qi(1))
-            z = w[:, self.i] + (R / (np.tan(e / 2))) * qi
-
-            if in_half_plane(p, z, qi):
-                if self.i < (N - 1):
-                    self.i = self.i + 1
-                self.state = 1
-
-        else:
-            # Fly north as default
-            flag = -1
-            r = p
-            q = mat([1, 0, 0]).T
-            c = np.nan(3, 1)
-            rho = np.nan
-            lamb = np.nan
-
-        return flag, r, q, c, rho, lamb
-
-    def findDubinsParameters(self, p_s, chi_s, p_e, chi_e, R):
-        """
-        findDubinsParameters determines the dubins path parameters
-
-        Inputs:
-        p_s = start position (m)
-        chi_s = start course angle (rad)
-        p_e = end position (m)
-        chi_e = end course angle (rad)
-        R = turn radius (m)
-
-        Outputs
-        dp.L = path length (m)
-        dp.c_s = start circle origin (m)
-        dp.lamb_s = start circle direction (unitless)
-        dp.c_e = end circle origin (m)
-        dp.lamb_e = end circle direction (unitless)
-        dp.z_1 = Half-plane H_1 location (m)
-        dp.q_12 = Half-planes H_1 and H_2 unit normals (unitless)
-        dp.z_2 = Half-plane H_2 location (m)
-        dp.z_3 = Half-plane H_3 location  (m)
-        dp.q_3 = Half-plane H_3 unit normal (m)
-        dp.case = case (unitless)
-
-        Example Usage
-        dp = findDubinsParameters( p_s, chi_s, p_e, chi_e, R )
-
-        Reference: Beard, Small Unmanned Aircraft, Chapter 11, Algorithm 7
-        Copyright 2018 Utah State University
-        """
-
-        # TODO Algorithm 7 goes here
-
-        # package output into DubinsParameters class
-        dp = DubinsParameters()
-
-        # TODO populate dp members here
-
-        return dp
-
-    def followWppDubins(self, W, Chi, p, R, newpath):
-        """
-        followWppDubins implements waypoint following via Dubins paths
-
-        Inputs:
-            W = list of waypoints in NED (m)
-            Chi = list of course angles at waypoints in NED (rad)
-            p = mav position in NED (m)
-            R = fillet radius (m)
-            newpath = flag to initialize the algorithm or define new waypoints
-
-        Outputs
-            flag = flag for straight line path (1) or orbit (2)
-            r = origin of straight-line path in NED (m)
-            q = direction of straight-line path in NED (m)
-            c = center of orbit in NED (m)
-            rho = radius of orbit (m)
-            lamb = direction or orbit, 1 clockwise, -1 counter clockwise
-            self.i = waypoint number
-            dp = dubins path parameters
-
-        Example Usage
-            flag, r, q, c, rho, lamb = followWppDubins(W, Chi, p, R, newpath)
-
-        Reference: Beard, Small Unmanned Aircraft, Chapter 11, Algorithm 8
-        Copyright 2018 Utah State University
-        """
-
-        # TODO Algorithm 8 goes here
-
-        return flag, r, q, c, rho, lamb, self.i, dp
+#     def followWpp(self, w, p, newpath):
 
 
-class DubinsParameters:
-    def __init__(self):
-        """
-        Member Variables:
-            L = path length (m)
-            c_s = start circle origin (m)
-            lamb_s = start circle direction (unitless)
-            c_e = end circle origin (m)
-            lamb_e = end circle direction (unitless)
-            z_1 = Half-plane H_1 location (m)
-            q_1 = Half-planes H_1 and H_2 unit normals (unitless)
-            z_2 = Half-plane H_2 location (m)
-            z_3 = Half-plane H_3 location  (m)
-            q_3 = Half-plane H_3 unit normal (m)
-            case = case (unitless)
-        """
 
-        self.L = 0
-        self.c_s = mat([0, 0, 0]).T
-        self.lamb_s = -1
-        self.c_e = mat([0, 0, 0]).T
-        self.lamb_e = 1
-        self.z_1 = mat([0, 0, 0]).T
-        self.q_1 = mat([0, 0, 0]).T
-        self.z_2 = mat([0, 0, 0]).T
-        self.z_3 = mat([0, 0, 0]).T
-        self.q_3 = mat([0, 0, 0]).T
-        self.case = 0
-        self.lengths = np.array([[0, 0, 0, 0]])
-        self.theta = 0
-        self.ell = 0
-        self.c_rs = mat([0, 0, 0]).T
-        self.c_ls = mat([0, 0, 0]).T
-        self.c_re = mat([0, 0, 0]).T
-        self.c_le = mat([0, 0, 0]).T
+
+
+
+#         """
+#         followWpp implements waypoint following via connected straight-line
+#         paths.
+
+#         Inputs:
+#             w = 3xn matrix of waypoints in NED (m)
+#             p = position of MAV in NED (m)
+#             newpath = flag to initialize the algorithm or define new waypoints
+
+#         Outputs
+#             r = origin of straight-line path in NED (m)
+#             q = direction of straight-line path in NED (m)
+
+#         Example Usage;
+#             r, q = followWpp(w, p, newpath)
+
+#         Reference: Beard, Small Unmanned Aircraft, Chapter 11, Algorithm 5
+#         Copyright 2018 Utah State University
+#         """
+
+#         if self.i is None:
+#             self.i = 0
+
+#         if newpath:
+#             # initialize index
+#             self.i = 1
+
+#         # check sizes
+#         m, N = w.shape
+#         assert N >= 3
+#         assert m == 3
+
+#         # calculate the q vector
+#         r = w[:, self.i - 1]
+#         qi1 = s_norm(w[:, self.i], -w[:, self.i - 1])
+
+#         # Calculate the origin of the current path
+#         qi = s_norm(w[:, self.i + 1], -w[:, self.i])
+
+#         # Calculate the unit normal to define the half plane
+#         ni = s_norm(qi1, qi)
+
+#         # Check if the MAV has crossed the half-plane
+#         if in_half_plane(p, w[:, self.i], ni):
+#             if self.i < (N - 2):
+#                 self.i += 1
+#         q = qi1
+#         return r, q
+
+#     # followWppFillet algorithm left here for reference.
+#     # It is not used in the final implementation
+#     def followWppFillet(self, w, p, R, newpath):
+#         """
+#         followWppFillet implements waypoint following via straightline paths
+#         connected by fillets
+
+#         Inputs:
+#             W = 3xn matrix of waypoints in NED (m)
+#             p = position of MAV in NED (m)
+#             R = fillet radius (m)
+#             newpath = flag to initialize the algorithm or define new waypoints
+
+#         Outputs
+#             flag = flag for straight line path (1) or orbit (2)
+#             r = origin of straight-line path in NED (m)
+#             q = direction of straight-line path in NED (m)
+#             c = center of orbit in NED (m)
+#             rho = radius of orbit (m)
+#             lamb = direction or orbit, 1 clockwise, -1 counter clockwise
+
+#         Example Usage
+#             [flag, r, q, c, rho, lamb] = followWppFillet( w, p, R, newpath )
+
+#         Reference: Beard, Small Unmanned Aircraft, Chapter 11, Algorithm 6
+#         Copyright 2018 Utah State University
+#         """
+
+#         if self.i is None:
+#             self.i = 0
+#             self.state = 0
+#         if newpath:
+#             # Initialize the waypoint index
+#             self.i = 2
+#             self.state = 1
+
+#             # Check size of waypoints matrix
+#             m, N = w.shape  # Where 'N' is the number of waypoints and 'm' dimensions
+#             assert N >= 3
+#             assert m == 3
+#         else:
+#             [m, N] = w.shape
+#             assert N >= 3
+#             assert m == 3
+#         # Calculate the q vector and fillet angle
+#         qi1 = mat(s_norm(w[:, self.i], -w[:, self.i - 1]))
+#         qi = mat(s_norm(w[:, self.i + 1], -w[:, self.i]))
+#         e = acos(-qi1.T * qi)
+
+#         # Determine if the MAV is on a straight or orbit path
+#         if self.state == 1:
+#             c = mat([0, 0, 0]).T
+#             rho = 0
+#             lamb = 0
+
+#             flag = 1
+#             r = w[:, self.i - 1]
+#             q = q1
+#             z = w[:, self.i] - (R / (np.tan(e / 2))) * qi1
+#             if in_half_plane(p, z, qi1):
+#                 self.state = 2
+
+#         elif self.state == 2:
+#             r = [0, 0, 0]
+#             q = [0, 0, 0]
+
+#             flag = 2
+#             c = w[:, self.i] - (R / (np.sin(e / 2))) * s_norm(qi1, -qi)
+#             rho = R
+#             lamb = np.sign(qi1(1) * qi(2) - qi1(2) * qi(1))
+#             z = w[:, self.i] + (R / (np.tan(e / 2))) * qi
+
+#             if in_half_plane(p, z, qi):
+#                 if self.i < (N - 1):
+#                     self.i = self.i + 1
+#                 self.state = 1
+
+#         else:
+#             # Fly north as default
+#             flag = -1
+#             r = p
+#             q = mat([1, 0, 0]).T
+#             c = np.nan(3, 1)
+#             rho = np.nan
+#             lamb = np.nan
+
+#         return flag, r, q, c, rho, lamb
+
+#     def findDubinsParameters(self, p_s, chi_s, p_e, chi_e, R):
+#         """
+#         findDubinsParameters determines the dubins path parameters
+
+#         Inputs:
+#         p_s = start position (m)
+#         chi_s = start course angle (rad)
+#         p_e = end position (m)
+#         chi_e = end course angle (rad)
+#         R = turn radius (m)
+
+#         Outputs
+#         dp.L = path length (m)
+#         dp.c_s = start circle origin (m)
+#         dp.lamb_s = start circle direction (unitless)
+#         dp.c_e = end circle origin (m)
+#         dp.lamb_e = end circle direction (unitless)
+#         dp.z_1 = Half-plane H_1 location (m)
+#         dp.q_12 = Half-planes H_1 and H_2 unit normals (unitless)
+#         dp.z_2 = Half-plane H_2 location (m)
+#         dp.z_3 = Half-plane H_3 location  (m)
+#         dp.q_3 = Half-plane H_3 unit normal (m)
+#         dp.case = case (unitless)
+
+#         Example Usage
+#         dp = findDubinsParameters( p_s, chi_s, p_e, chi_e, R )
+
+#         Reference: Beard, Small Unmanned Aircraft, Chapter 11, Algorithm 7
+#         Copyright 2018 Utah State University
+#         """
+
+#         # TODO Algorithm 7 goes here
+
+#         # package output into DubinsParameters class
+#         dp = DubinsParameters()
+
+#         # TODO populate dp members here
+
+#         return dp
+
+#     def followWppDubins(self, W, Chi, p, R, newpath):
+#         """
+#         followWppDubins implements waypoint following via Dubins paths
+
+#         Inputs:
+#             W = list of waypoints in NED (m)
+#             Chi = list of course angles at waypoints in NED (rad)
+#             p = mav position in NED (m)
+#             R = fillet radius (m)
+#             newpath = flag to initialize the algorithm or define new waypoints
+
+#         Outputs
+#             flag = flag for straight line path (1) or orbit (2)
+#             r = origin of straight-line path in NED (m)
+#             q = direction of straight-line path in NED (m)
+#             c = center of orbit in NED (m)
+#             rho = radius of orbit (m)
+#             lamb = direction or orbit, 1 clockwise, -1 counter clockwise
+#             self.i = waypoint number
+#             dp = dubins path parameters
+
+#         Example Usage
+#             flag, r, q, c, rho, lamb = followWppDubins(W, Chi, p, R, newpath)
+
+#         Reference: Beard, Small Unmanned Aircraft, Chapter 11, Algorithm 8
+#         Copyright 2018 Utah State University
+#         """
+
+#         # TODO Algorithm 8 goes here
+
+#         return flag, r, q, c, rho, lamb, self.i, dp
+
+
+# class DubinsParameters:
+#     def __init__(self):
+#         """
+#         Member Variables:
+#             L = path length (m)
+#             c_s = start circle origin (m)
+#             lamb_s = start circle direction (unitless)
+#             c_e = end circle origin (m)
+#             lamb_e = end circle direction (unitless)
+#             z_1 = Half-plane H_1 location (m)
+#             q_1 = Half-planes H_1 and H_2 unit normals (unitless)
+#             z_2 = Half-plane H_2 location (m)
+#             z_3 = Half-plane H_3 location  (m)
+#             q_3 = Half-plane H_3 unit normal (m)
+#             case = case (unitless)
+#         """
+
+#         self.L = 0
+#         self.c_s = mat([0, 0, 0]).T
+#         self.lamb_s = -1
+#         self.c_e = mat([0, 0, 0]).T
+#         self.lamb_e = 1
+#         self.z_1 = mat([0, 0, 0]).T
+#         self.q_1 = mat([0, 0, 0]).T
+#         self.z_2 = mat([0, 0, 0]).T
+#         self.z_3 = mat([0, 0, 0]).T
+#         self.q_3 = mat([0, 0, 0]).T
+#         self.case = 0
+#         self.lengths = np.array([[0, 0, 0, 0]])
+#         self.theta = 0
+#         self.ell = 0
+#         self.c_rs = mat([0, 0, 0]).T
+#         self.c_ls = mat([0, 0, 0]).T
+#         self.c_re = mat([0, 0, 0]).T
+#         self.c_le = mat([0, 0, 0]).T
 
 
